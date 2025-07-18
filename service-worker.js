5c5a1396061ee5787e211a9ddce89ad4991d37e0
MatiasKV0 <159393104+MatiasKV0@users.noreply.github.com>
2025-07-18 19:14:51 -0300

    Add service worker for caching assets and handling fetch requests

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('mi-app').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/style.css',
+        '/script.js',
+        '/public/logo.webp'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
